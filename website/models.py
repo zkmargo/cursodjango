97ff42392c0ec965332f3aa8471f95338b15093f
zkmargo <zkmargo@gmail.com>
2022-12-18 10:21:21 -0300

    First commit

---
+++ website/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=200)
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.title
